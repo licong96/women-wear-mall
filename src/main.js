8182c82bd0d339d4f9e4c5c649fa198c353fbef1
licong <licong19961204@gmail.com>
2017-11-15 21:46:43 +0800

    add console.log()

--- src/main.js
+++ src/main.js
@@ -47,3 +47,6 @@ new Vue({
   template: '<App/>',
   components: { App }
 })
+
+console.log('%c开发者：黎聪 ', 'background-image:-webkit-gradient( linear, left top, right top, color-stop(0, #f22), color-stop(0.15, #f2f), color-stop(0.3, #22f), color-stop(0.45, #2ff), color-stop(0.6, #FF3355),color-stop(0.75, #22f), color-stop(0.9, #ff7459), color-stop(1, #ff7459) );color:transparent;-webkit-background-clip: text;font-size:24px;')
+console.log('%cGitHub：https://github.com/licong96', 'text-shadow: 0 1px 0 #ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15);font-size:16px')
